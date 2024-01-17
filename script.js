700d6c748086814911b9b98b4682a035660910b4
KevinJMasson <kevin.j.masson@gmail.com>
2024-01-17 15:23:21 -0700

    Implemented full Etch-a-sketch functionality

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ const grid = document.querySelector('#grid');
 const gridSize = 960;
 gridNum = 16;
 
+drawGrid();
 
-for (let i = 0; i<gridNum;i++) {
-    var row = document.createElement('div');
-    row.classList.add('row');
-    for (let k = 0; k < gridNum; k++) {
-        var square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
+const sizeButton = document.querySelector('button');
+sizeButton.addEventListener('click', () => {
+    gridNum = parseInt(prompt("Please enter a new number of squares for the grid"));
+    grid.innerHTML = '';
+    drawGrid();
+})
+
+function drawGrid () {
+    for (let i = 0; i<gridNum;i++) {
+        var row = document.createElement('div');
+        row.classList.add('row');
+        for (let k = 0; k < gridNum; k++) {
+            var square = document.createElement('div');
+            square.classList.add('square');
+            square.style.height = gridSize/gridNum + 'px';
+            square.style.width = gridSize/gridNum + 'px';
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
-}
-
-const squares = document.querySelectorAll('.square');
-
-squares.forEach((square) => {
-    square.addEventListener('mouseover', function(e) {
-        e.target.style.background = 'black';
+    
+    const squares = document.querySelectorAll('.square');
+    
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', function(e) {
+            e.target.style.background = 'black';
+        });
     });
-});
+}
